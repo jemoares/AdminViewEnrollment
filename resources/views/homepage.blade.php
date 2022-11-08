9da42f5d6ef9d5aeb9413ca25b794108efe47c59
jemoares <jheremie.magat32@gmail.com>
2022-11-08 19:46:59 +0800

    initial commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="viewport" content="with=device-width, initial-scale=1.0">
+        <title> AMLAC Senior High School Enrollment</title>
+        <!----======== CSS ======== -->
+        <link href="/css/home.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+
+<script src="https://kit.fontawesome.com/72ceecc077.js" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body>
+
+    
+        <section class="header">
+            <nav>
+                <a href="/homepage"><img src="/images/channels4_profile.jpg"></a>
+                <div class="nav-links" id="navLinks">
+                    <i class="fa fa-solid fa-circle-xmark" onclick="hideMenu()"></i>
+                    <ul>
+                        <li><a href="">Home</a> </li>
+                        <li><a href="">Course</a> </li>
+                        <li><a href="{{('/about')}}">About</a> </li>
+                        <li><a href="">Contact</a> </li>
+                        <li><a href="{{('/admin/login')}}">Login as admin </a></li>
+                        <li><a href="{{('/Login')}}">Login as student </a></li>
+                    </ul>
+
+                </div>
+                <i class="fa fa-solid fa-bars-progress" onclick="showMenu()"></i>
+            </nav>
+            <div class="text-box">
+                <h1>Amlac Senior High School</h1>
+            </div>
+        </section>
+<!------ INNOVATION --------->
+
+<!------ INNOVATION --------->
+<section class="innovation">
+        <h1>Innovation</h1>
+        
+
+        <div class="row">
+    <div class="innovation-col">
+        <h3>Inform</h3>
+        <p>This way, we feel it’s easiest to accumulate new knowledge and reshape our existing understanding on different areas of innovation.</p>
+
+    </div>
+
+    <div class="innovation-col">
+        <h3>Connect</h3>
+        <p>Self-discipline is an act of cultivation. It requires you to connect today's actions to tomorrow's results. There's a season for sowing a season for reaping. Self-discipline helps you know which is which</p>
+
+    </div>
+
+    <div class="innovation-col">
+        <h3>Transform</h3>
+        <p>To improve is to change to be perfect.</p>
+
+    </div>
+
+    </section>
+        <!------ Course --------->
+        <section class="course">
+            <h1>Courses Offer</h1>
+     
+        </section>
+
+<section class="Amlac" id="Courses">
+    <div class="container">
+        <div class="Amlac" style="max-width:600px">
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="box-wrap">
+                    <div class="icon">
+                        <i class="fas fa-duotone fa-calculator blue-clr-bg"></i>
+                    </div>
+                    <h4 class="number">A</h4>
+                    <h4><a href="#url">ABM</a></h4>
+                    <p>Accountancy, Business, and Management (ABM) strand. may be the track for you. 
+                    This Senior High School strand will introduce you to the fundamentals of business, accounting, marketing, and economics.
+                    It will also provide you knowledge and skills in line with the digital.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+                <div class="box-wrap">
+                    <div class="icon">
+                        <i class="fas fa-sharp fa-solid fa-gears primary-clr-bg"></i>
+                        
+                    </div>
+                    <h4 class="number">B</h4>
+                    <h4><a href="#url">STEM</a></h4>
+                    <p>STEM is an educational program developed to prepare primary and secondary students for college, 
+                        graduate study and careers in the fields of science, technology, engineering and mathematics (STEM). 
+                        In addition to subject-specific learning, STEM aims to foster inquiring minds, logical reasoning and 
+                        collaboration skills.</p>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-lg-0 mt-4">
+                <div class="box-wrap">
+                    <div class="icon">
+                        <i class="fas fa-sharp fa-solid fa-user-graduate green-clr-bg"></i>
+                        
+                    </div>
+                    <h4 class="number">C</h4>
+                    <h4><a href="#url">GAS</a></h4>
+                    <p> General Academic Strand. Through the GAS, senior high school
+                         students are given exposure to different courses so they can 
+                         make a more informed decision about their future career path.</p>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4">
+                <div class="box-wrap">
+                    <div class="icon">
+                        <i class="fas fa-sharp fa-solid fa-users-between-lines primary-clr-bg"></i>
+                        
+                    </div>
+                    <h4 class="number">D</h4>
+                    <h4><a href="#url">HUMS</a></h4>
+                    <p>The Humanities and Social Sciences (HUMSS) strands equip students with a wide range of discipline 
+                        with the use of their experiences and skills into the investigation and inquiry of human situations 
+                        by studying its behavior and social changes using empirical, analytical, and critical method techniques.</p>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4">
+                <div class="box-wrap">
+                    <div class="icon">
+                        <i class="fas fa-user-cog green-clr-bg"></i>
+                    </div>
+                    <h4 class="number">E</h4>
+                    <h4><a href="#url">TVL</a></h4>
+                    <p>TVL Strand is designed to develop students' skills that is useful for livelihood and technical projects. 
+                        It provides a curriculum that is a combination of Core Courses and specialized hands-on courses that meets the 
+                        competency-based assessment of TESDA.</p>
+                   
+                </div>
+            </div>
+           
+            </div>
+        </div>
+    </div>
+</section>
+<!----------Enrollment Here------------->
+<section class="Enrollment">
+    <h1>Open enrollment <br>The Future Begins Here!</h1>
+    <a href="/enrollment" class="hero-btn">Enroll Now</a>
+</section>
+
+<!----------FOOTER------------->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>company</h4>
+                <ul>
+                    <li><a href="#">Course</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Help</h4>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Address</h4>
+                <ul>
+                <p style="color:#bbbbbb";>Paseo del Carmen St. Amlac Ville Subdivision, Payatas B. Quezon City </p>
+                        
+                    </p>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>follow us</h4>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    <!-----JavaScript for Toggle Menu--->
+        <script>
+            var navLinks = document.getElementById("navLinks");
+
+            function showMenu(){
+                navLinks.style.right = "0";
+            }
+
+            function hideMenu(){
+                navLinks.style.right = "-200px";
+            }
+
+
+        </script>
+        {!!menu('Admin-Panel')!!}
+    </body>
+</html>
